8c3801ef11b39440155a11ba5a80d99259cfd137
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-08-29 14:09:40 -0500

    Created initial dashboard and added header and footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Dashbord from "./pages/Dashboard";
+import Post from "./pages/Post";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-    
+    <div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Dashbord />} />
+      <Route  path="/posts/:id" element={<Post/>}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
     </div>
   );
 }
